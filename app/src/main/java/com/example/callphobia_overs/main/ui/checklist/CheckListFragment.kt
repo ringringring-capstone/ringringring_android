08ba05cc4c37c86b1fa1085c83f9bad23213aff2
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-04-06 22:32:35 +0900

    add : 메인화면들 추가

---
+++ app/src/main/java/com/example/callphobia_overs/main/ui/checklist/CheckListFragment.kt
@@ -0,0 +1,21 @@
+package com.example.callphobia_overs.main.ui.checklist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.callphobia_overs.R
+
+class CheckListFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_checklist)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
