d3ba0bdf0668bb120df42a81f7179d357cfbb473
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-04-16 12:34:30 +0900

    fix : login xml 수정

--- app/src/main/java/com/example/callphobia_overs/main/ui/member/loginFragment.kt
+++ app/src/main/java/com/example/callphobia_overs/main/ui/member/loginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.callphobia_overs.main.ui.member
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.callphobia_overs.R
 import com.example.callphobia_overs.databinding.ActivityLoginFragmentBinding
+import com.example.callphobia_overs.main.ui.home.HomeFragment
 
 class loginFragment : AppCompatActivity() {
 
@@ -16,6 +18,10 @@ class loginFragment : AppCompatActivity() {
         binding = ActivityLoginFragmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeFragment::class.java)
+            startActivity(intent)
+        }
 
 
     }
