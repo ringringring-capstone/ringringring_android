d7dcc26c7530d597c3b18c11dfa53bc3942a4bf4
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-04-18 21:57:31 +0900

    add: fragment base 추가

---
+++ app/src/main/java/com/example/callphobia_overs/main/baseFragment.kt
@@ -0,0 +1,37 @@
+package com.example.callphobia_overs.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+/** fragment base, 중복되는 부분들 없앰 */
+
+abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes : Int) : Fragment() {
+    lateinit var binding : T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =  DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.lifecycleOwner = viewLifecycleOwner
+        initView()
+        initClick()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    abstract fun initView() //보여주는 부분 작성
+    abstract fun initClick() //클릭 이벤트 부분 작성
+
+}
