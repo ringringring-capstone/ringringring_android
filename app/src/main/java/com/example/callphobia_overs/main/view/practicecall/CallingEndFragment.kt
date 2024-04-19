d123e2c78679e6d6b5078f3358fb0171549845ec
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-04-19 22:41:40 +0900

    add : 통화 fragment 추가

---
+++ app/src/main/java/com/example/callphobia_overs/main/view/practicecall/CallingEndFragment.kt
@@ -0,0 +1,15 @@
+package com.example.callphobia_overs.main.view.practicecall
+
+import com.example.callphobia_overs.R
+import com.example.callphobia_overs.databinding.FragmentCallingEndBinding
+import com.example.callphobia_overs.main.BaseFragment
+
+class CallingEndFragment : BaseFragment<FragmentCallingEndBinding>(R.layout.fragment_calling_end) {
+    override fun initClick() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
---
+++ app/src/main/java/com/example/callphobia_overs/main/view/practicecall/CallingFragment.kt
@@ -0,0 +1,20 @@
+package com.example.callphobia_overs.main.view.practicecall
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.callphobia_overs.R
+import com.example.callphobia_overs.databinding.FragmentCallingBinding
+import com.example.callphobia_overs.main.BaseFragment
+
+class CallingFragment : BaseFragment<FragmentCallingBinding>(R.layout.fragment_calling) {
+    override fun initClick() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
