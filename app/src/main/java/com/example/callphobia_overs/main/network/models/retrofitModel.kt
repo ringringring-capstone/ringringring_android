2a3900abb522961dd11cad6c49332d170c0a5038
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-04-23 22:39:29 +0900

    fix : retrofit 싱글톤으로 변경

--- app/src/main/java/com/example/callphobia_overs/main/network/models/retrofitModel.kt
+++ app/src/main/java/com/example/callphobia_overs/main/network/models/retrofitModel.kt
@@ -1,12 +1,21 @@
 package com.example.callphobia_overs.main.network.models
 
 import com.example.callphobia_overs.main.network.api.RingApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import javax.inject.Singleton
 
+/**참고 블로그 : https://jtm0609.tistory.com/22*/
+
+@Module
+@InstallIn(SingletonComponent::class)
 object retrofitModel {
 
     private val baseUrl = "http://localhost:9090" //4.14일 기준 임시로 작성해둠
@@ -15,15 +24,23 @@ object retrofitModel {
         level = HttpLoggingInterceptor.Level.BODY //okhttp 로그 추가
     }
 
-    private val okHttp = OkHttpClient.Builder()
-        .addInterceptor(logging)
-        .build()
+    @Singleton
+    @Provides
+    fun OkHttpClient() : OkHttpClient{
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .client(okHttp)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(RingApi::class.java)
+    @Singleton
+    @Provides
+    fun ringRetrofit() : RingApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient())
+            .baseUrl(baseUrl)
+            .build()
+            .create(RingApi::class.java)
+    }
 
 }
