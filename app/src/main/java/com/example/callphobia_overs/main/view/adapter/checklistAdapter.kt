6402317769ace70182cc66f503f03e0c97deefff
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-04-25 21:31:03 +0900

    add : 체크리스트 데이터 추가

---
+++ app/src/main/java/com/example/callphobia_overs/main/view/adapter/checklistAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.callphobia_overs.main.view.adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.example.callphobia_overs.R
+import com.example.callphobia_overs.databinding.RowChecklistBinding
+
+class checklistAdapter (val context: Context, val checkList: ArrayList<checkDataClass>): BaseAdapter() {
+    private var binding : RowChecklistBinding? = null
+    override fun getCount(): Int {
+        return checkList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return checkList[position]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return 0
+    }
+
+    @SuppressLint("ViewHolder", "InflateParams")
+    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
+        binding = RowChecklistBinding.inflate(LayoutInflater.from(context))
+        val data = checkList[position]
+        binding!!.rowChecklistContent.text = data.content
+
+        return binding!!.root
+
+    }
+
+}
