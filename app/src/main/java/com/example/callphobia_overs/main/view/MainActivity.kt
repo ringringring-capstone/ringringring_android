af2db134d037e0def0edd9d174ce318fdcbeb67d
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-03 22:21:45 +0900

    fix : 네비게이션 이동시 튕기는 현상 수정

--- app/src/main/java/com/example/callphobia_overs/main/view/MainActivity.kt
+++ app/src/main/java/com/example/callphobia_overs/main/view/MainActivity.kt
@@ -3,53 +3,69 @@ package com.example.callphobia_overs.main.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.callphobia_overs.R
 import com.example.callphobia_overs.databinding.ActivityMainBinding
 import com.example.callphobia_overs.main.view.checklist.CheckListResultFragment
-import com.example.callphobia_overs.main.view.checklist.CheckListTestFragment
 import com.example.callphobia_overs.main.view.home.HomeFragment
 import com.example.callphobia_overs.main.view.setting.SettingFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-    private val manager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setBottomNavigation()
+       // binding.mainBottomNav.selectedItemId = R.id.fragment_home
 
+        setBottomNavigation()
     }
 
     private fun setBottomNavigation(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.mainBottomNav.setupWithNavController(navController)
+
+        binding.mainBottomNav.selectedItemId = R.id.fragment_home
+
         binding.mainBottomNav.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.fragment_checkList -> {
-                    CheckListResultFragment().changeFragment()
+                    navController.navigate(R.id.checkListResultFragment)
                 }
                 R.id.fragment_setting -> {
-                    SettingFragment().changeFragment()
+                    //changeFragment(SettingFragment())
+                    navController.navigate(R.id.settingFragment)
+                }
+                R.id.fragment_home -> {
+                    navController.navigate(R.id.homeFragment)
                 }
             }
             return@setOnItemSelectedListener true
         }
 
         binding.goHome.setOnClickListener {
-            HomeFragment().changeFragment()
+            navController.navigate(R.id.homeFragment)
+            binding.mainBottomNav.selectedItemId = R.id.fragment_home
         }
 
         binding.mainBottomNav.setOnItemReselectedListener {  }
     }
 
-    private fun Fragment.changeFragment() {
-        manager.beginTransaction().replace(R.id.showFrame, this).commitAllowingStateLoss()
-    }
-
-    public fun replaceFragment(fragment: Fragment){
+    /*
+    private fun changeFragment(fragment: Fragment) {
         supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragContainer, fragment)
+            .commit()
     }
+*/
 }
--- app/src/main/java/com/example/callphobia_overs/main/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/callphobia_overs/main/view/home/HomeFragment.kt
@@ -2,29 +2,43 @@ package com.example.callphobia_overs.main.view.home
 
 import android.util.Log
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.callphobia_overs.R
 import com.example.callphobia_overs.databinding.FragmentHomeBinding
 import com.example.callphobia_overs.main.base.BaseFragment
+import com.example.callphobia_overs.main.view.practicecall.CallingFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
-    lateinit var navController: NavController
+
+
     override fun initClick() { //뭘 누르던간 다 전화통화 화면으로 이동해야함
+
         binding.groupHomeMenu.btnReservation.setOnClickListener { //예약
-            findNavController().navigate(R.id.callingFragment)
+            requireActivity().findNavController(R.id.fragContainer).navigate(R.id.action_homeFragment_to_callingFragment)
+            //findNavController().navigate(R.id.action_homeFragment_to_callingFragment)
         }
 
         binding.groupHomeMenu.btnDelivery.setOnClickListener { //배달
-            findNavController().navigate(R.id.callingFragment)
+            requireActivity().findNavController(R.id.fragContainer).navigate(R.id.action_homeFragment_to_callingFragment)
+            //findNavController().navigate(R.id.action_homeFragment_to_callingFragment)
+            //NavHostFragment.findNavController(CallingFragment())
         }
 
         binding.groupHomeMenu.btnInquiry.setOnClickListener { //상담
-            findNavController().navigate(R.id.callingFragment)
+            requireActivity().findNavController(R.id.fragContainer).navigate(R.id.action_homeFragment_to_callingFragment)
+            //findNavController().navigate(R.id.action_homeFragment_to_callingFragment)
         }
 
         binding.groupHomeMenu.btnMisson.setOnClickListener { //미션버튼
             Log.d("미션버튼 누름","미션버튼")
-            findNavController().navigate(R.id.missionMainFragment)
+            requireActivity().findNavController(R.id.fragContainer).navigate(R.id.action_homeFragment_to_missionMainFragment)
+            //findNavController().navigate(R.id.action_homeFragment_to_missionMainFragment)
         }
 
     }
@@ -32,6 +46,5 @@ class HomeFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun initView() {
 
     }
-
 
 }
