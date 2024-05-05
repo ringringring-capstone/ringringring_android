1a2c90e6f803305c2fabd76acca48dddc547149e
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-05 22:40:39 +0900

    add : 회원가입 완료시 화면 추가

--- app/src/main/java/com/example/callphobia_overs/main/view/member/membershipActivity.kt
+++ app/src/main/java/com/example/callphobia_overs/main/view/member/membershipActivity.kt
@@ -1,10 +1,12 @@
 package com.example.callphobia_overs.main.view.member
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.example.callphobia_overs.R
 import com.example.callphobia_overs.databinding.ActivityMembershipFragmentBinding
 import com.example.callphobia_overs.main.network.viewmodel.DataViewModel
@@ -20,15 +22,34 @@ class membershipActivity : AppCompatActivity() {
     private val vm by viewModels<DataViewModel>()
     private val LOG = "membership"
 
+    var name = ""
+    var email = ""
+    var pw = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMembershipFragmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        name = binding.etUserName.text.toString()
+        email = binding.etUserEmail.text.toString()
+        pw = binding.etUserPwd.text.toString()
+
+        binding.run {
+            textLayoutName.run {
+                editText?.run {
+                    addTextChangedListener {
+                        if(name.isEmpty()){
+                            textLayoutName.error = "이름을 입력해주세요."
+                        } else {
+                            textLayoutName.error = null
+                        }
+                    }
+                }
+            }
+        }
+
         binding.btnMembership.setOnClickListener {
-            val name = binding.etUserName.text.toString()
-            val email = binding.etUserEmail.text.toString()
-            val pw = binding.etUserPwd.text.toString()
 
             if(name.isEmpty() || email.isEmpty() || pw.isEmpty()){
                 Toast.makeText(this, "정보를 모두 입력해주세요", Toast.LENGTH_SHORT).show()
@@ -39,8 +60,11 @@ class membershipActivity : AppCompatActivity() {
 
                     if(result){
                         runOnUiThread {
-                            Toast.makeText(this@membershipActivity, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                            //Toast.makeText(this@membershipActivity, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
                             Log.d(LOG, "회원가입 성공")
+                            val intent = Intent(this@membershipActivity, membershipComplete::class.java)
+                            startActivity(intent)
+
                         }
                     }
                 }
@@ -49,7 +73,25 @@ class membershipActivity : AppCompatActivity() {
 
     }
 
-    private fun checkEmail(email : String) {
+    private fun checkName() {
 
+    }
+
+    private fun checkEmail(email : String) : Boolean {
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+
+        if(email.isEmpty()){
+            binding.textLayoutEmail.error = "메일을 입력해주세요."
+
+        } else if(!email.matches(emailPattern.toRegex())){
+            binding.textLayoutEmail.error = "이메일 형식에 맞춰 작성해주세요."
+
+        } else {
+            binding.textLayoutEmail.error = null
+            binding.textLayoutEmail.isErrorEnabled = false
+            return true
+        }
+
+        return false
     }
 }
--- app/src/main/java/com/example/callphobia_overs/main/view/member/membershipComplete.kt
+++ app/src/main/java/com/example/callphobia_overs/main/view/member/membershipComplete.kt
@@ -1,5 +1,6 @@
 package com.example.callphobia_overs.main.view.member
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +14,12 @@ class membershipComplete : AppCompatActivity() {
     private lateinit var binding : ActivityMembershipCompleteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_membership_complete)
+        binding = ActivityMembershipCompleteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnCompleteLogin.setOnClickListener {
+            val intent = Intent(this, loginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
