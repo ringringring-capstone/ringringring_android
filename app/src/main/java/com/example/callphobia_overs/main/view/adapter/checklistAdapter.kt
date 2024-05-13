6a4b7b1437c72f55d77685a3900cb89f71f5d94c
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-13 21:13:33 +0900

    add : checklist 체크개수 리턴 추가

--- app/src/main/java/com/example/callphobia_overs/main/view/adapter/checklistAdapter.kt
+++ app/src/main/java/com/example/callphobia_overs/main/view/adapter/checklistAdapter.kt
@@ -12,6 +12,7 @@ import com.example.callphobia_overs.databinding.RowChecklistBinding
 
 class checklistAdapter (val context: Context, val checkList: ArrayList<checkDataClass>): BaseAdapter() {
     private var binding : RowChecklistBinding? = null
+    private var checkCount = 0
     override fun getCount(): Int {
         return checkList.size
     }
@@ -29,10 +30,25 @@ class checklistAdapter (val context: Context, val checkList: ArrayList<checkData
         binding = RowChecklistBinding.inflate(LayoutInflater.from(context))
         val data = checkList[position]
         binding!!.rowChecklistContent.text = data.content
-        Log.d("체크리스트내용",data.content)
-
+        binding!!.rowChecklistContent.isChecked = data.isCheck //기본 false로 해둠
+        
+        binding!!.rowChecklistContent.setOnCheckedChangeListener { _, ischeck ->
+            data.isCheck = ischeck
+            updateCheckCount()
+        }
+        
         return binding!!.root
 
+    }
+
+    fun getCheckedCount(): Int {
+        Log.d("CheckCount", checkCount.toString())
+        return checkCount
+    }
+
+    // 선택된 체크박스의 개수를 업데이트하는 메서드
+    private fun updateCheckCount() {
+        checkCount = checkList.count { it.isCheck }
     }
 
 }
