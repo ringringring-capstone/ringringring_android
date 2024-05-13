28ab8700b6cf6342dcc43b4f8c283f1ad1ebb5b9
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-13 21:14:01 +0900

    add : 체크리스트 저장용 sharedPreference 추가

---
+++ app/src/main/java/com/example/callphobia_overs/main/core/SharedPreference.kt
@@ -0,0 +1,27 @@
+package com.example.callphobia_overs.main.core
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**체크 리스트 확인 용도*/
+class SharedPreference (context: Context) {
+    private val pref = context.getSharedPreferences("checkList", Context.MODE_PRIVATE)
+    private val percentPerf = context.getSharedPreferences("percent", Context.MODE_PRIVATE)
+
+    fun getIsCheck(key : String) : Boolean {
+        return pref.getBoolean(key,false) //기본으로 false 가져옴
+    }
+
+    fun setIsCheck(key : String, defValue: Boolean){
+        pref.edit().putBoolean(key, defValue).apply()
+    }
+
+    fun getCheckListPerCent(key : String) : Int {
+        return percentPerf.getInt(key, 0)
+    }
+
+    fun setCheckListPerCent(key : String, setValue : Int){
+        percentPerf.edit().putInt(key, setValue).apply()
+    }
+}
--- app/src/main/java/com/example/callphobia_overs/main/core/application.kt
+++ app/src/main/java/com/example/callphobia_overs/main/core/application.kt
@@ -5,7 +5,12 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class Application  : Application(){
+
+    companion object{
+        lateinit var preferManager: SharedPreference
+    }
     override fun onCreate() {
+        preferManager = SharedPreference(applicationContext)
         super.onCreate()
     }
 }
