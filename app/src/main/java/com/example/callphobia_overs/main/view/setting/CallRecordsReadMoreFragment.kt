106ea2411847fe886e0812651ddb9304fcdf0580
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-23 21:12:44 +0900

    add : 통화내역 자세히 보기 화면 추가

---
+++ app/src/main/java/com/example/callphobia_overs/main/view/setting/CallRecordsReadMoreFragment.kt
@@ -0,0 +1,21 @@
+package com.example.callphobia_overs.main.view.setting
+
+import android.os.Bundle
+import com.example.callphobia_overs.R
+import com.example.callphobia_overs.databinding.FragmentCallRecordsReadMoreBinding
+import com.example.callphobia_overs.main.base.BaseFragment
+
+class CallRecordsReadMoreFragment : BaseFragment<FragmentCallRecordsReadMoreBinding>(R.layout.fragment_call_records_read_more) {
+
+    private val LOG = "readmoreFragment"
+
+    override fun initView() {
+
+
+    }
+
+    override fun initClick() {
+
+    }
+
+}
