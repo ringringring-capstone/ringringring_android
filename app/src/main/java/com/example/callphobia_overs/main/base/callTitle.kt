626f400a829de8223e6801a7e8779f12d70fc416
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-26 22:58:58 +0900

    add : ai 통화 주제 추가

---
+++ app/src/main/java/com/example/callphobia_overs/main/base/callTitle.kt
@@ -0,0 +1,9 @@
+package com.example.callphobia_overs.main.base
+
+class CallTitle {
+    val deliverTitle = listOf("짜장면 배달 주문 시작", "치킨 배달 주문 시작", "햄버거 배달 주문 시작")
+
+    val reservationTitle = listOf("중식당 전화 예약 연습 시작", "막창 전화 예약 연습 시작", "파스타 전화 예약 연습 시작")
+
+    val refundTitle = listOf("옷 환불 전화 연습 시작", "택배 환불 전화 연습 시작")
+}
