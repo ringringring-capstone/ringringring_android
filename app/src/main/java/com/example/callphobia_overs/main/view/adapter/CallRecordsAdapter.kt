dc9541921a9d8e4d8545a1c768d2865d97d2a575
NDN <83949732+Najungmin510@users.noreply.github.com>
2024-05-28 18:13:13 +0900

    add : 통화내역 삭제기능 추가

--- app/src/main/java/com/example/callphobia_overs/main/view/adapter/CallRecordsAdapter.kt
+++ app/src/main/java/com/example/callphobia_overs/main/view/adapter/CallRecordsAdapter.kt
@@ -6,16 +6,22 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.activity.viewModels
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.example.callphobia_overs.R
 import com.example.callphobia_overs.databinding.RowCallingHistoryBinding
 import com.example.callphobia_overs.main.network.models.roomDB.CallRecords
+import com.example.callphobia_overs.main.network.viewmodel.DataViewModel
 
 /**roomDB에서 데이터 전부 가져오기 참고 : https://bumjae.tistory.com/47#AddActivity-1 */
 
+interface DeleteListener{
+    fun delete(item : CallRecords)
+}
 
-
-class CallRecordsAdapter(val context : Context) : RecyclerView.Adapter<CallRecordsAdapter.Holder>() { //dataclass 따로 만들 필요 없이, 그냥 DB에 사용했던거 쓰면 됨
+class CallRecordsAdapter(val context : Context,
+    private val deleteListener: DeleteListener) : RecyclerView.Adapter<CallRecordsAdapter.Holder>() { //dataclass 따로 만들 필요 없이, 그냥 DB에 사용했던거 쓰면 됨
 
     private var dataList : List<CallRecords> = listOf()
     private val LOG = "callRecordApdater"
@@ -51,11 +57,11 @@ class CallRecordsAdapter(val context : Context) : RecyclerView.Adapter<CallRecor
                 val popup = PopupMenu(binding.root.context, it)
                 popup.menuInflater.inflate(R.menu.callhistory_menu_option, popup.menu)
 
-                popup.setOnMenuItemClickListener {item ->
-                    when(item.itemId){
+                popup.setOnMenuItemClickListener {menuItem ->
+                    when(menuItem.itemId){
                         R.id.option_delete -> {
                             Log.d(LOG, "삭제버튼 클릭")
-
+                            deleteListener.delete(item)
                             true
                         }
                         else -> {
--- app/src/main/java/com/example/callphobia_overs/main/view/setting/CallRecordsFragment.kt
+++ app/src/main/java/com/example/callphobia_overs/main/view/setting/CallRecordsFragment.kt
@@ -3,19 +3,31 @@ package com.example.callphobia_overs.main.view.setting
 import android.annotation.SuppressLint
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.callphobia_overs.R
 import com.example.callphobia_overs.databinding.FragmentCallRecordsBinding
 import com.example.callphobia_overs.main.base.BaseFragment
+import com.example.callphobia_overs.main.network.models.roomDB.CallRecords
 import com.example.callphobia_overs.main.network.viewmodel.DataViewModel
 import com.example.callphobia_overs.main.view.adapter.CallRecordsAdapter
+import com.example.callphobia_overs.main.view.adapter.DeleteListener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CallRecordsFragment : BaseFragment<FragmentCallRecordsBinding>(R.layout.fragment_call_records) {
 
     private val vm : DataViewModel by activityViewModels()
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun initView() {
-        val adapter = CallRecordsAdapter(requireContext())
+        val adapter = CallRecordsAdapter(requireContext(), object : DeleteListener{
+            override fun delete(item: CallRecords) {
+                lifecycleScope.launch(Dispatchers.IO) {
+                    vm.callRecordDelete(item)
+                }
+            }
+        })
+
         binding.recyclerCallRecords.adapter = adapter
         binding.recyclerCallRecords.layoutManager = LinearLayoutManager(context)
 
@@ -23,13 +35,12 @@ class CallRecordsFragment : BaseFragment<FragmentCallRecordsBinding>(R.layout.fr
             adapter.setData(it)
         })
 
+
         binding.toolbarRecords.run {
             title = "통화 내역 버튼"
             setNavigationIcon(R.drawable.btn_back_black)
         }
-
         deleteRecord(adapter)
-
     }
 
     override fun initClick() {
